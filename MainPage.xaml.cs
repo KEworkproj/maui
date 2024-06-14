bb43f35b11146c017ab0805551ce3ad7d3abb430
mwas-arch <cmkaguri@outlook.com>
2024-06-14 21:01:58 +0300

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿namespace phoneword
+{
+	public partial class MainPage : ContentPage
+	{
+
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		public MainPage()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		{
+			InitializeComponent();
+		}
+
+		string translatedNumber;
+
+		private void OnTranslate(object sender, EventArgs e)
+		{
+			string enteredNumber = PhoneNumberText.Text;
+			translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+			if (!string.IsNullOrEmpty(translatedNumber))
+			{
+				// TODO:enables the button if the test is true
+
+				CallButton.IsEnabled = true;
+				CallButton.Text = "Call " + translatedNumber;
+			}
+			else
+			{
+				// TODO:dis able the button
+				CallButton.IsEnabled = false;
+				CallButton.Text = "Call";
+			}
+		
+
+			}
+		async void OnCall(object sender, System.EventArgs e)
+		{
+			if (await this.DisplayAlert(
+					"Dial a Number",
+					"Would you like to call " + translatedNumber + "?",
+					"Yes",
+					"No"))
+			{
+				// TODO: dial the phone
+				try
+				{
+					if (PhoneDialer.Default.IsSupported)
+						PhoneDialer.Default.Open(translatedNumber);
+				}
+				catch (ArgumentNullException)
+				{
+					await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+				}
+				catch (Exception)
+				{
+					// Other error has occurred.
+					await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+				}
+			}
+		}
+	}
+
+}
---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("phoneword.MainApplication, phoneword, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488db191822441c55.MainApplication.class, crc6488db191822441c55.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
